deb1ef3c8877eaeb30e566221b031a0f9f5b5b3c
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-08-20 01:47:20 +0200

    add some nixpkgs

--- system/nixpkgs/default.nix
+++ system/nixpkgs/default.nix
@@ -31,6 +31,30 @@
     statix
     nixfmt-rfc-style
 
+    # cli-utils
+    silver-searcher # ag - recursive string search in directory
+    tldr # better man
+    scc # count code lines
+    duf # disk usage - better df
+    dua # disk usage - better du
+    aria2 # download utility - better wget
+    bat # better cat
+    diff-so-fancy # better diff
+    fzf # fuzzy find
+    jq # json processor
+    most # better more/less
+    procs # better ps
+    rsync # incremental file transfer
+    sd # simpler sed
+    gping # better ping
+    speedtest-cli # internet speed test
+
+    # TODO: configure with Wiki
+    # https://nixos.wiki/wiki/Docker
+    # docker
+    # docker-compose
+    # lazydocker
+
     # misc
     unzip
     gnumake
