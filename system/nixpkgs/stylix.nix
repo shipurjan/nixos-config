8fd3dad804efa801e44c382259621499ae3f5996
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-08-24 02:16:08 +0200

    add default fonts to stylix

--- system/nixpkgs/stylix.nix
+++ system/nixpkgs/stylix.nix
@@ -1,7 +1,30 @@
+{ pkgs, ... }:
 {
   stylix = {
     enable = true;
     image = ../../images/bitches-brew.jpg;
     polarity = "dark";
+
+    fonts = {
+      serif = {
+        package = pkgs.gyre-fonts;
+        name = "TeX Gyre Schola";
+      };
+
+      sansSerif = {
+        package = pkgs.gyre-fonts;
+        name = "TeX Gyre Heros";
+      };
+
+      monospace = {
+        package = pkgs.fira-code;
+        name = "Fira Code";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 }
