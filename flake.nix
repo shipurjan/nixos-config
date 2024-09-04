872f3840ede8de15b0268af0493bdb29e498cea9
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-09-04 23:00:44 +0200

    add nixos-unstable-small overlay
    for https://github.com/hyprwm/xdg-desktop-portal-hyprland/issues/256

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -26,14 +27,33 @@
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }@inputs:
+    {
+      nixpkgs,
+      home-manager,
+      nixos-unstable-small,
+      ...
+    }@inputs:
     let
+      # TODO: remove nixos-unstable-small and this overlay
+      # when https://github.com/hyprwm/xdg-desktop-portal-hyprland/issues/256
+      # works on unstable
+      unstable-small-pkgs = import nixos-unstable-small { inherit system; };
+      xdphOverlay = final: prev: {
+        inherit (unstable-small-pkgs) xdg-desktop-portal-hyprland;
+      };
       system = "x86_64-linux";
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        overlays = [ xdphOverlay ];
+        config = {
+          allowUnfree = true;
+        };
+      };
     in
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         specialArgs = {
-          inherit inputs system;
+          inherit inputs system pkgs;
         };
         modules = [ ./system ];
       };
